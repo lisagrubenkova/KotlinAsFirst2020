25552e385b3ccc0adb1aec46e44b10cae9eea7e0
Lisa <elizaveta.grubenkova@gmail.com>
2020-09-25 00:56:03 +0300

    выполнила lesson2 task2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,9 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -18,7 +21,8 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean =
+    number / 1000 + number / 100 % 10 == number / 10 % 10 + number % 10
 
 /**
  * Простая (2 балла)
@@ -27,7 +31,8 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2)
 
 
 /**
@@ -36,7 +41,13 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int =
+    when {
+        year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && month == 2 -> 29
+        month == 2 -> 28
+        month == 4 || month == 6 || month == 9 || month == 11 -> 30
+        else -> 31
+    }
 
 /**
  * Простая (2 балла)
@@ -59,4 +70,5 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int) =
+    (a <= r || b <= r || c <= r) && (a <= s || b <= s || c <= s)
