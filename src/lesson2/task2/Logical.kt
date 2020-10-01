04198654a95ed46476659b6772e021f89a8d2be2
Lisa <elizaveta.grubenkova@gmail.com>
2020-10-01 23:21:22 +0300

    исправила ошибку

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -46,7 +46,7 @@ fun daysInMonth(month: Int, year: Int): Int =
         4, 6, 9, 11 -> 30
         1, 3, 5, 7, 8, 10, 12 -> 31
         else -> {
-            if ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && month == 2) 29
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) 29
             else 28
         }
     }
